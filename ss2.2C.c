7bf1d142ba2e9b8f65027546c8aa3cf52af46c58
minh-duy2608 <nguyenduy26806@gmail.com>
2024-11-21 13:50:38 +0700

    first commit

---
+++ ss2.2C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    //Khai bao va khoi tao hai bien kieu int
+    int a = 5;     //Bien a duoc khoi tao voi gia tri la 5 
+    int b = 10;   //Bien b duoc khoi tao voi gia tri la 10 
+    
+    //Khai bao mot bien kieu int de luu ket qua tong 
+    int sum = a + b;  //Tinh tong cua a va b, luu vao bien sum 
+    
+    //In ket qua ra man hinh 
+    printf("Tong cua %d va %d la %d\n", a, b, sum);  //In ra tong cua a va b 
+    
+    return 0;
+}
